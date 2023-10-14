9df1bec44198430a2ee4f442d5b301f900268d07
Rodney <dabrerodneyy@gmail.com>
2023-10-14 23:43:47 +0530

    done

--- countdownTimer/main.js
+++ countdownTimer/main.js
@@ -1,37 +1,23 @@
-const endDate = "20 October 2023 11:00:00 AM"
+const endDate = "20 October 2023 11:00:00 AM";
 
 document.getElementById("end-date").innerText = endDate;
-const inputs = document.querySelectorAll("input")
-    // const clock = () => {
-
-// }
+const inputs = document.querySelectorAll("input");
 
 function clock() {
-    const end = new Date(endDate)
-    const now = new Date()
-    const diff = (end - now) / 1000;
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
 
-    if (diff < 0) return;
+  if (diff < 0) return;
 
-    // convert into days;
-    inputs[0].value = Math.floor(diff / 3600 / 24);
-    inputs[1].value = Math.floor(diff / 3600) % 24;
-    inputs[2].value = Math.floor(diff / 60) % 60;
-    inputs[3].value = Math.floor(diff) % 60;
+  inputs[0].value = Math.floor(diff / 3600 / 24);
+  inputs[1].value = Math.floor(diff / 3600) % 24;
+  inputs[2].value = Math.floor(diff / 60) % 60;
+  inputs[3].value = Math.floor(diff) % 60;
 }
 
-// initial call
-clock()
+clock();
 
-/**
- *  1 day = 24 hrs
- *  1 hr = 60 mins
- *  60 min = 3600 sec
- */
-
-setInterval(
-    () => {
-        clock()
-    },
-    1000
-)
+setInterval(() => {
+  clock();
+}, 1000);
